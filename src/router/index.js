37936fc36dd8259375a913a8e657b612ac5bc88a
wellspokenrambler <isabellecmay@gmail.com>
2024-05-27 08:58:57 +0100

    changes:
    - removed testimonials page and references to it as it proved unnecessary
    - added screenshots of vanellus demo to examples
    - prepared screenshots of brochure site

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,6 @@ import { createRouter, createWebHistory } from "vue-router"
 import Home from "../views/Home.vue"
 import About from "../views/About.vue"
 import Examples from "../views/Examples.vue"
-import Testimonials from "../views/Testimonials.vue"
 import Contact from "../views/Contact.vue"
 import CV from "../views/CV.vue"
 
@@ -13,7 +12,6 @@ const routes = [
   { path: "/", name: "Home", component: Home },
   { path: "/about", name: "About", component: About },
   { path: "/examples", name: "Examples", component: Examples },
-  { path: "/testimonials", name: "Testimonials", component: Testimonials },
   { path: "/contact", name: "Contact", component: Contact },
   { path: "/cv", name: "CV", component: CV}
 ];
--- src/views/Examples.vue
+++ src/views/Examples.vue
@@ -11,26 +11,42 @@
             <div class="w-full flex flex-col items-center">
                 <heading-three>2D Navier-Stokes Demo</heading-three>
 
-                <div class="w-full rounded-lg overflow-hidden mt-6">
-                    <video autoplay playsinline loop muted preload="auto">
-                        <source src="/vanellus-2D-navier-stokes-demo.mov">
-                    </video>
+                <PrimaryBodyCopy class="mt-4">
+                    A web-page I built while working at Vanellus Technologies Ltd, who specialised
+                    in machine-learning-aided complex fluid dynamics, to demonstrate their simulation
+                    of a lid-driven cavity flow.
+                    <br />
+                    <br />
+                    It was designed to accept values for the number of
+                    cells on the X and Y axis, the Reynolds number, and the length of time that the
+                    simulation should run for. Then, it would send these values to a server programmed
+                    in Python which calculated the vorticity of each cell based on the given values -
+                    iterating over the specified time - then returned multidimensional arrays of data which
+                    were used by the front-end to generate a series of time-stepped vorticity plots.
+                </PrimaryBodyCopy>
+                <HeadingSix class="mt-4 px-4">
+                    "Isi helped us get from 0-1 with our front-end, she was instrumental in
+                    helping us set up a range of beautiful prototype pages and components which
+                    are going to be critical for Vanellus when we launch our MVP."
+                </HeadingSix>
+                <HeadingFive class="mt-2">
+                    - Laurence Cullen, CEO Vanellus Technologies Ltd
+                </HeadingFive>
+
+                <div class="grid grid-rows-2 md:grid-rows-none md:grid-cols-2 gap-4 mt-6">
+                    <div class="w-full h-fit-content rounded-lg overflow-hidden">
+                        <img src="/vanellus-2D-navier-stokes-demo-1.PNG?url">
+                    </div>
+                    <div class="w-full rounded-lg overflow-hidden">
+                        <img src="/vanellus-2D-navier-stokes-demo-2.png?url">
+                    </div>
                 </div>
 
-                <PrimaryBodyCopy class="mt-4">
-                    A web-page I built while working at Vanellus Technologies Ltd, who specialised 
-                    in machine-learning-aided complex fluid dynamics, to demonstrate their simulation 
-                    of a lid-driven cavity flow.
-                    <br/> 
-                    <br/> 
-                    It was designed to accept values for the number of 
-                    cells on the X and Y axis, the Reynolds number, and the length of time that the
-                    simulation should run for. Then, it would send these values to a server programmed 
-                    in Python which calculated the vorticity of each cell based on the given values - 
-                    iterating over the specified time - then returned multidimensional arrays of data which 
-                    were used by the front-end to generate a series of time-stepped vorticity plots.
-                    <br/> 
-                    <br/> 
+                <div class="w-5/6 rounded-lg overflow-hidden mt-6">
+                    <img src="/vanellus-2D-navier-stokes-demo-3.png?url">
+                </div>
+
+                <PrimaryBodyCopy class="w-full mt-4">
                     Front-end tech stack: Vue 3, SASS, Tailwind CSS, Vite
                 </PrimaryBodyCopy>
             </div>
@@ -65,6 +81,8 @@ import AOS from "aos"
 import PrimaryCard from "../components/Cards/PrimaryCard.vue"
 import HeadingTwo from '../components/TypeScale/HeadingTwo.vue';
 import HeadingThree from "../components/TypeScale/HeadingThree.vue";
+import HeadingFive from "../components/TypeScale/HeadingFive.vue";
+import HeadingSix from "../components/TypeScale/HeadingSix.vue";
 import PrimaryBodyCopy from '../components/TypeScale/PrimaryBodyCopy.vue'
 
 AOS.init()
