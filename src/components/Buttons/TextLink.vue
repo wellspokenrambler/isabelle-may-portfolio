99457d9e2e993e0c9bbf13099e82b129b2331660
wellspokenrambler <36102623+wellspokenrambler@users.noreply.github.com>
2024-05-28 18:29:38 +0100

    changes:
    - added dark-mode specificity to text link
    - added typescript svg to files

--- src/components/Buttons/TextLink.vue
+++ src/components/Buttons/TextLink.vue
@@ -2,7 +2,7 @@
     <a
         :href="href"
         :target="target"
-        class="font-inconsolata text-base font-bold text-center text-white hover:text-blue active:text-blue-dark-20 underline decoration-2 underline-offset-4 transition duration-200">
+        class="font-inconsolata text-base font-bold text-center dark:text-white hover:text-blue active:text-blue-dark-20 underline decoration-2 underline-offset-4 transition duration-200">
         <slot></slot>
     </a>
 </template>
